d15a6fc371281da0776dbdfd60d9d17dc333d147
Sang Cu <sangcn@gmail.com>
2014-10-14 23:11:50 +0700

    add resources file

---
+++ assets/js/summer.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var summer = (function ($) {
+
+    var indexPostClass = '.summer-index-post',
+        mobileMenuButton = '.summer-mobile-menu a',
+        mobileMenuCloseButton = '.summer-mobile-close-btn',
+        mainMenu = '.summer-menu',
+        bgCheckClass = '.bg-check',
+        postBgImages = '.bg-img img',
+        postCoverImg = '.summer-post-header .bg-img',
+        tagsDiv = '.tags-loop',
+        allAnimationTypes = ['simple', 'sliced', 'push'],
+
+    // post animations on homepage
+    indexPostAnimate = function () {
+        if ($('.animated-post').length && $('.post-loop-animations').length) {
+            $(indexPostClass).each(function () {
+            var postPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop(),
+                windowHeight = $(window).height();
+                if (postPos < topOfWindow + (windowHeight/ 1.4)) {
+                    $(this).addClass('fadeInDown');
+                }
+            });
+        }
+    },
+
+    mobileMenu = function () {
+        if($(mainMenu).length) {
+            $(mobileMenuButton).on('click', function(e){
+                e.preventDefault();
+                $(mainMenu).addClass('opened');
+                $('header').height(200);
+            });
+            $(mobileMenuCloseButton).on('click', function(e){
+                e.preventDefault();
+                $(mainMenu).removeClass('opened');
+                $('header').height(0);
+            });
+        }
+    },
+
+    headerTitlesBackgroundCheck = function () {
+        if ($(bgCheckClass).length && $(postBgImages).length) {
+            imagesLoaded(postCoverImg, function () {
+                BackgroundCheck.init({
+                    targets: bgCheckClass,
+                    images: postBgImages
+                });
+            });
+        }
+    },
+
+    // for now only this nasty hacks can do this
+    // hiding animation types tags used to switching posts covers
+    removeSimpleTagHelper = function () {
+        if ($(tagsDiv).length) {
+            $(tagsDiv).each(function () {
+                var tagLink = $(this).find('.tag-link > a'),
+                    tagPrefix = $(this).find('.tag-prefix');
+                tagLink.each(function () {
+                    if (allAnimationTypes.indexOf($(this).text()) !== -1) {
+                        $(this).closest('.tag-link').remove();
+                    }
+                });
+                if(tagPrefix.next().length === 0) {
+                    tagPrefix.remove();
+                }
+            });
+        }
+    },
+
+    // summer javascripts initialization
+    init = function () {
+        indexPostAnimate();
+        $(window).on('scroll', function() {
+            indexPostAnimate();
+        });
+        mobileMenu();
+        headerTitlesBackgroundCheck();
+        $('p:has(> img)').addClass('with-image');
+        removeSimpleTagHelper();
+        $(document).foundation();
+    };
+
+    return {
+        init: init
+    };
+
+})(jQuery);
+
+(function () {
+    summer.init();
+})();
